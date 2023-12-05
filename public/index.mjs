fdeee66e4399e0d97e623dcbeed135303b19f4c3
Christian Bonte <cjbonte7@gmail.com>
2023-12-05 13:17:56 -0500

    Added randomize functionality and fonts

--- public/index.mjs
+++ public/index.mjs
@@ -3,22 +3,32 @@ import reviews from "./reviews.json" assert { type: 'json' };
 
 let reviewsPerId = 0;
 let pageIds = [];
+let pageIdElm = document.getElementById("page-ids");
+let totalReviewsElm = document.getElementById("total-reviews");
+let randomHelpful = document.getElementById("helpful-votes");
+let randomNotHelpful = document.getElementById("unhelpful-votes");
+let randomReco = document.getElementById("recommended");
+let randomSource = document.getElementById("source");
+
+
 
 //handle form submits
 function handleSubmit(e) {
-    e.preventDefault();
-
-    let pageIdElm = document.getElementById("page-ids");
-    let totalReviewsElm = document.getElementById("total-reviews");
+    e.preventDefault();    
 
     //pass the entered ids into a funciton that will add them into a new array
     addIds(pageIdElm.value);
     reviewsPerId = totalReviewsElm.value;
 
-    //Pass all the entered data and reveiew data into the csv maker
-    csvMaker(pageIds, reviewsPerId, reviews);
+    let helpful = randomHelpful.checked;
+    let notHelpful = randomNotHelpful.checked;
+    let reco = randomReco.checked;
+    let source = randomSource.checked;
 
-    alert("A CSV was successfully generated!")
+    //Pass all the entered data and reveiew data into the csv maker
+    csvMaker(pageIds, reviewsPerId, reviews, helpful, notHelpful, reco, source);
+
+    //alert("A CSV was successfully generated!")
 }
 
 //split the ids and add them into the pageIds array
@@ -29,7 +39,7 @@ function addIds(ids) {
     }
 }
 
-function csvMaker(pageIds, reviewsPerId, reviews) {
+function csvMaker(pageIds, reviewsPerId, reviews, helpful, notHelpful, reco, source) {
 
     let reviewsWithIds = [];
 
@@ -37,11 +47,39 @@ function csvMaker(pageIds, reviewsPerId, reviews) {
     if (pageIds !== null && reviewsPerId !== null) {
 
         let reviewCounter = 0;
+        let helpfulNum = 0;
+        let notHelpfulNum = 0;
+
+        let recommendArr = [];
+        let recoNum = 0;
+
+        let sourceArr = [];
+        let sourceNum = 0;
+
+        
 
         //loop through each entered page id
         for(let i = 0; i < pageIds.length; i++) {
             //loop until it reaches the reviews per page id value
             for(let j = 0; j < reviewsPerId; j++) {
+
+                if (helpful === true) {
+                    helpfulNum = Math.floor(Math.random() * 50);
+                }
+        
+                if (notHelpful === true) {
+                    notHelpfulNum = Math.floor(Math.random() * 50);
+                }
+        
+                if (reco === true) {
+                    recommendArr = ["Yes", "No"];
+                    recoNum = Math.round(Math.random());
+                }
+                
+                if (source === true) {
+                    sourceArr = ["email", "web"];
+                    sourceNum = Math.round(Math.random());
+                }
                 
                 // break out if review counter ever exceeds the length of reviews array
                 if(reviewCounter >= reviews.length) {
@@ -53,6 +91,10 @@ function csvMaker(pageIds, reviewsPerId, reviews) {
                     reviews[reviewCounter]["review status"] = "Approved";
                     reviews[reviewCounter]["review title"] = reviews[reviewCounter]["review title"].replace(",", "");
                     reviews[reviewCounter]["review text"] = reviews[reviewCounter]["review text"].replace(",", "");
+                    reviews[reviewCounter]["helpful votes"] = helpfulNum;
+                    reviews[reviewCounter]["not helpful votes"] = notHelpfulNum;
+                    reviews[reviewCounter]["recommended"] = recommendArr[recoNum];
+                    reviews[reviewCounter]["review source"] = sourceArr[sourceNum];
                     reviewsWithIds.push(reviews[reviewCounter]);
                     ++reviewCounter;
                     
